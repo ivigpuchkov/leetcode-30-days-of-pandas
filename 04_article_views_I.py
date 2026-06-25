07cfd41e63c58f4d275588a039df0ed0b1934d3d
Ivan Puchkov <ivigpuchkov@mail.ru>
2026-06-25 16:58:24 +0300

    решение 04_article_views_I.py: фильтрация, сортировка и удаление повторов

---
+++ 04_article_views_I.py
@@ -0,0 +1,32 @@
+"""
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date.
+Note that equal author_id and viewer_id indicate the same person.
+
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+"""
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return(
+        views
+        .query("author_id == viewer_id")
+        .rename(columns={'author_id':'id'})
+        .loc[:,['id']]
+        .drop_duplicates('id')
+        .sort_values('id')
+    )
