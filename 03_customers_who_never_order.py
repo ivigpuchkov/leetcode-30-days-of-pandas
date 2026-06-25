a9bbed9237e5acdcddefd2a8f3e872f477c4ac3c
Ivan Puchkov <ivigpuchkov@mail.ru>
2026-06-25 16:56:48 +0300

    решение 03_customers_who_never_order.py: слияние таблиц, фильтрация строк

---
+++ 03_customers_who_never_order.py
@@ -0,0 +1,41 @@
+"""
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+
+The result format is in the following example.
+"""
+
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    return (
+        customers
+        .merge(orders, how='left', left_on='id', right_on='customerId', suffixes=('_cust', '_ord'))
+        .query("id_ord.isna()")
+        .loc[:,['name']]
+        .rename(columns={'name':'Customers'})
+    )
