012879ea897d89d1b15da3c6733321cef21324af
Ivan Puchkov <ivigpuchkov@mail.ru>
2026-06-26 14:28:11 +0300

    решение 25_replace_employee_ID_with_the_unnique_identifier.py: left-join

---
+++ 25_replace_employee_ID_with_the_unnique_identifier.py
@@ -0,0 +1,32 @@
+"""
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+"""
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    return(
+        pd.merge(employees, employee_uni, how='left', on='id')
+        .loc[:,['unique_id', 'name']]
+    )
