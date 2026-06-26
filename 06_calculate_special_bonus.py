d38d4da4317d14aa91b3bf170c2a72c14a1b9185
Ivan Puchkov <ivigpuchkov@mail.ru>
2026-06-26 10:58:30 +0300

    решение 06_calculate_special_bonus.py: добавление столбца, метод .get()

---
+++ 06_calculate_special_bonus.py
@@ -0,0 +1,27 @@
+"""
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key (column with unique values) for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+
+
+Write a solution to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee's name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+
+The result format is in the following example.
+"""
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    return (
+        employees
+        .assign(bonus=employees['salary']*(employees['name'].str.get(0)!='M')*(employees['employee_id']%2!=0))
+        .sort_values(['employee_id'])
+        .loc[:,['employee_id','bonus']]
+    )
