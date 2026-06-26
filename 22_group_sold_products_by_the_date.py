3eca08e3dc7b2f54f6676d13544530d8b81765f4
Ivan Puchkov <ivigpuchkov@mail.ru>
2026-06-26 14:19:10 +0300

    решение 22_group_sold_products_by_the_date.py: именованное агрегирование (метод .agg())

---
+++ 22_group_sold_products_by_the_date.py
@@ -0,0 +1,30 @@
+"""
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+
+
+Write a solution to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+"""
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    return(
+        activities.drop_duplicates()
+        .sort_values(['sell_date', 'product'])
+        .groupby('sell_date')
+        .agg(
+            num_sold = ('product', 'count'),
+            products = ('product', ','.join)
+        )
+        .reset_index()
+    )
