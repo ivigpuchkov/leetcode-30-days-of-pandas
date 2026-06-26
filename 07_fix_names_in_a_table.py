1c832bc61ec75809f3a8c45ac8764ebdeec1ac59
Ivan Puchkov <ivigpuchkov@mail.ru>
2026-06-26 10:59:49 +0300

    решение 07_fix_names_in_a_table.py: метод .capitalize()

---
+++ 07_fix_names_in_a_table.py
@@ -0,0 +1,25 @@
+"""
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+"""
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    return (
+        users
+        .assign(name=users['name'].str.capitalize())
+        .sort_values(['user_id'])
+    )
